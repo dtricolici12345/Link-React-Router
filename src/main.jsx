99e6a671ad90dc579988820508a35e5cc056068b
Daniela Tricolici <dtricolici12345@gmail.com>
2023-12-22 11:37:07 +0100

    utiliser les link

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,36 @@
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+
 // page components
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Article from "./pages/Article";
 
 // router creation
 
 const router = createBrowserRouter([
   {
-    element:<App />,
+    element: <App />,
     children: [
       {
         path: "/",
-        element: <Home />
+        element: <Home />,
       },
       {
         path: "/about",
-        element: <About />
+        element: <About />,
+      },
+      {
+        path: "/articles/:id",
+        element: <Article />,
       },
     ],
   },
 ]);
+
 // rendering
 
 ReactDOM.createRoot(document.getElementById("root")).render(
